5a49b2797ce34e4f871f3146270ce2d5130e82e9
Duke Takle <duke.takle@gmail.com>
2024-01-23 12:02:32 -0600

    Bump version: 0.0.1 → 0.0.2

--- lambdautil/command.py
+++ lambdautil/command.py
@@ -29,7 +29,7 @@ lambdatool deploy. The lambda has been started in main.py.
 
 
 @click.group()
-@click.version_option(version='0.0.1')
+@click.version_option(version='0.0.2')
 def cli():
     pass
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 setup(
     name='LambdaUtil',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     description='Python Lambda Utility',
     author='Duke P. Takle',
