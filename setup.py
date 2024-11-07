968bc28128f572ab946ab3669204cf13468515ea
Duke Takle (ceres) <duke.takle@corteva.com>
2024-11-07 13:25:42 -0600

    Bump version: 0.3.0 → 0.4.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 setup(
     name='LambdaUtil',
-    version='0.3.0',
+    version='0.4.0',
     packages=find_packages(),
     description='Python Lambda Utility',
     author='Duke P. Takle',
