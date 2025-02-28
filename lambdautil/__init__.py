9f9140ee824dcc0c2a27ca2be329414acc476f78
Chuck Muckamuck <chuck.muckamuck@gmail.com>
2025-02-28 13:29:36 -0600

    Bump version: 0.4.4 → 0.4.5

--- lambdautil/__init__.py
+++ lambdautil/__init__.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 __title__ = 'lambdatool'
-__version__ = '0.4.4'
+__version__ = '0.4.5'
 __author__ = 'Duke P. Takle'
 __author_email__ = 'duke.takle@gmail.com'
 __license__ = '''MIT License
